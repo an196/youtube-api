cdb76a6264b51d28ca0971b0c8a19a7a82a34715
an196 <an9x0010@gmail.com>
2023-02-20 23:23:31 +0700

    add origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:4200',
-    'https://youtube-eta-ten.vercel.app'
+    'https://youtube-eta-ten.vercel.app',
+    'https://youtube-an196.vercel.app'
 ];
 
 module.exports = allowedOrigins;
